049868699ce0c6306e6ce17bddd092f8ddd94202
han <qitmac000404@qitmac000404deMacBook-Pro.local>
2018-08-03 15:38:43 +0800

    change axios

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
                     <li><Link to="/dishEdit">菜品test录入</Link></li>
                     <li><Link to="/showChart">报表</Link></li>
                     <li><Link to="/import">数据导入</Link></li>
-                    <li><Link to="/perform">绩效数据</Link></li>
+                    <li><Link to="/nav/perform">绩效数据</Link></li>
                 </ul>  
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -19,11 +19,11 @@ ReactDOM.render((
   <Router>    
       <div>
         <Route path="/dishEdit" component={DishEdit}/>  
-        <Route path="/nav" component={App}/>  
+        <Route path="/nav/home" component={App}/>  
         <Route path="/showChart" component={Chart}/>  
         <Route path="/import" component={Import}/>  
         <Route path="/dishConsume/:dishId/:consumeList/:dishName" component={DishConsumeEdit}/>  
-        <Route path="/perform" component={WeekData}/>  
+        <Route path="/nav/perform" component={WeekData}/>  
         {/* <Route path="/genChart" component={GenChart}/>   */}
       </div>    
    </Router>
